8736487dd0b832bc6eee11320afb2b1daec4cacc
Mark Clifford <markmclifford@gmail.com>
2023-08-27 08:55:21 -0700

    full project push

--- index.js
+++ index.js
@@ -30,65 +30,6 @@ const auth = new google.auth.JWT(
 // Your TIMEOFFSET Offset
 const TIMEOFFSET = '-08:00';
 
-// Get date-time string for calender
-// const dateTimeForCalander = () => {
-
-//     let date = new Date();
-//     // console.log(date)
-
-//     let year = date.getFullYear();
-//     let month = date.getMonth() + 1;
-//     if (month < 10) {
-//         month = `0${month}`;
-//     }
-//     let day = date.getDate();
-//     if (day < 10) {
-//         day = `0${day}`;
-//     }
-//     let hour = date.getHours();
-//     if (hour < 10) {
-//         hour = `0${hour}`;
-//     }
-//     let minute = date.getMinutes();
-//     if (minute < 10) {
-//         minute = `0${minute}`;
-//     }
-
-//     let newDateTime = `${year}-${month}-${day}T${hour}:${minute}:00.000${TIMEOFFSET}`;
-
-//     let event = new Date(Date.parse(newDateTime));
-
-//     let startDate = event;
-//     // Delay in end time is 1
-//     let endDate = new Date(new Date(startDate).setHours(startDate.getHours()+1));
-
-//     return {
-//         'start': startDate,
-//         'end': endDate
-//     }
-// };
-
-// console.log(dateTimeForCalander());
-
-
-
-// let dateTime = dateTimeForCalander();
-
-// Event for Google Calendar
-// let event = {
-//     'summary': `This is the summary.`,
-//     'description': `This is the description.`,
-//     'start': {
-//         'dateTime': dateTime['start'],
-//         'timeZone': 'Asia/Kolkata'
-//     },
-//     'end': {
-//         'dateTime': dateTime['end'],
-//         'timeZone': 'Asia/Kolkata'
-//     }
-// };
-
-
 
 // Get all the events between two dates
 
@@ -107,8 +48,8 @@ const getEvents = async (dateTimeStart, dateTimeEnd) => {
         let items = response['data']['items'];
         return items;
     } catch (error) {
-        console.log(`Error at getEvents --> ${error}`);
-        return 0;
+        console.error("Error at getEvents:", error);
+        return null;
     }
 };
 
